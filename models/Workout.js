43cf90b25479e297c0f304c985f5ad03467770ad
Noah Nielsen <83135214+Sumtwelve@users.noreply.github.com>
2023-03-27 20:32:59 -0600

    add models
    User, Routine, and Workout models added.

---
+++ models/Workout.js
@@ -0,0 +1,38 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Workout extends Model {}
+
+Workout.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        instructions: {
+            type: DataTypes.STRING
+        },
+        routine_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'routine',
+                key: 'id'
+            }
+        },
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'routine',
+    }
+);
+
+module.exports = Workout;
