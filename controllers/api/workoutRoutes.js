f084eb3c86d464bcbac85350f64737ac923768d3
Gabe Faber <gmfaber55@gmail.com>
2023-03-29 18:35:42 -0600

    set up some possible routes

--- controllers/api/workoutRoutes.js
+++ controllers/api/workoutRoutes.js
@@ -3,7 +3,38 @@ const { Workout } = require('../../models');
 const withAuth = require('../../utils/auth');
 
 
+router.post('/', withAuth, async (req, res) => {
+    try {
+        const workoutData = await Workout.create({
+            ...req.body,
+            user_id: req.session.user_id,
+        });
 
+        res.status(200).json(workoutData);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+router.delete('/:id', withAuth, async (req, res) => {
+    try {
+        const workoutData = await Workout.destroy({
+            where: {
+                id: req.params.id,
+                user_id: req.session.user_id,
+            },
+        });
+
+        if (!worKoutData) {
+            res.status(404).json({ message: 'No workout found with this id!' });
+            return;
+        }
+
+        res.status(200).json(worKoutData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 
 
